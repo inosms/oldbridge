91a7cac932236772071eb0f717b8ab2382ba7966
inosms <kurisumasupurezento@gmail.com>
2018-03-30 15:06:28 +0200

    added author

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 
+
 setup(name='version_bridge',
       version='0.1',
       description='Call python code with a different python version',
       url='http://github.com/inosms/version_bridge',
       license='MIT',
+      author="inosms",
       packages=['version_bridge'],
       zip_safe=False)
